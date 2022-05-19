e3a32f1007759255e06a7b44cf432759a62f059d
mukesh111 <66948644+mukesh61112@users.noreply.github.com>
2022-05-19 06:48:20 +0530

    Create Pythagorean Triplet.java

---
+++ Pythagorean Triplet.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        // code here
+    
+           for (int i = 0; i < n; i++)
+            arr[i] = arr[i] * arr[i];
+ 
+        // Sort array elements
+        Arrays.sort(arr);
+ 
+        // Now fix one element one by one and find the other two
+        // elements
+        for (int i = n - 1; i >= 2; i--) {
+            // To find the other two elements, start two index
+            // variables from two corners of the array and move
+            // them toward each other
+            int l = 0; // index of the first element in arr[0..i-1]
+            int r = i - 1; // index of the last element in arr[0..i-1]
+            while (l < r) {
+                // A triplet found
+                if (arr[l] + arr[r] == arr[i])
+                    return true;
+ 
+                // Else either move 'l' or 'r'
+                if (arr[l] + arr[r] < arr[i])
+                    l++;
+                else
+                    r--;
+            }
+        }
+ 
+        // If we reach here, then no triplet found
+        return false; 
+    }
+}
