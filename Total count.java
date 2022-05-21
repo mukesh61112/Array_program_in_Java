cc957fe8468d4ab75898b6b916e48a3a56bc65d6
mukesh111 <66948644+mukesh61112@users.noreply.github.com>
2022-05-21 12:27:26 +0530

    Create Total count.java

---
+++ Total count.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().totalCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int totalCount(int[] arr, int n, int k) {
+        // code here
+        int sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            if(arr[i]%k==0 && arr[i]!=0)
+               count=arr[i]/k;
+            else if(arr[i]==0)
+              count=arr[i]/k +1;
+            else
+              count=arr[i]/k+1;
+              
+            sum=sum+count;
+        }
+        return sum;
+    }
+}
