ade10bf7440ef2506c4f68b0f12eeee214eec5f2
mukesh111 <66948644+mukesh61112@users.noreply.github.com>
2022-05-22 11:30:43 +0530

    Create First and last occurrences of X.java

---
+++ First and last occurrences of X.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            String inputLine[] = br.readLine().trim().split(" "); 
+            int n = Integer.parseInt(inputLine[0]); 
+            int x = Integer.parseInt(inputLine[1]);
+            
+            int arr[] = new int[(int)(n)];
+            
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.firstAndLast(arr, n, x); 
+            
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    } 
+} 
+            
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    public ArrayList<Integer> firstAndLast(int arr[], int n, int x){
+        // Code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        int l=-1;
+        boolean temp=true;
+        for(int i=0;i<n;i++)
+        {
+            if(temp && arr[i]==x)
+            {
+               ans.add(i);
+               temp=false;
+            }
+            if(!temp && arr[i]==x)
+            {
+                l=i;
+            }
+              
+        }
+        ans.add(l);
+        return ans;
+    }
+}
